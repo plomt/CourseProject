d1c0bd5154770440ebcbf17f17d7575f884ceb4b
Aleksandr Kondakov <bimboterminator@gmail.com>
2022-03-07 20:19:18 +0300

    File separation

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import sys
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from PyQt5.QtQml import QQmlApplicationEngine, qmlRegisterType
+from PyQt5.QtQuick import QQuickPaintedItem, QQuickItem
 
+from textRender import MyTerm
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    qmlRegisterType(MyTerm, 'Terminal', 1, 0, 'MyTerm')
+    engine = QQmlApplicationEngine('./qml/base.qml')
+    sys.exit(app.exec_())
